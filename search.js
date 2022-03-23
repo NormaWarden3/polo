57d0a427e87032b425e5b68e12adc79bb5f8afe8
NormaWarden3 <102206688+NormaWarden3@users.noreply.github.com>
2022-03-23 08:26:25 +0300

    Create search.js

---
+++ search.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+    document.writeln("Файл: "+document.URL+"<br>");
+    document.writeln("Последнее обновление:  "+document.lastModified+"<br>");
+    document.writeln("Размер файла: "+document.fileSize+" байт<br>");
+</script>
